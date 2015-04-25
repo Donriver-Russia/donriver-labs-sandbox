0cf4d6712022c880bc3cb658167344e44ac91df4
Ruslan08 <eto_ya08@mail.ru>
2015-04-25 23:20:05 +0300

    test commit

--- spring-ioc/weather-sensor-gateway/src/main/java/com/donriver/labs/sandbox/servlets/weather/gateway/controllers/GatewayController.java
+++ spring-ioc/weather-sensor-gateway/src/main/java/com/donriver/labs/sandbox/servlets/weather/gateway/controllers/GatewayController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
